5139c5528a5bb24d9277e73851c434ebf77cf8ab
Hunter T <30395560+StrangeRanger@users.noreply.github.com>
2018-01-14 20:36:08 -0800

    Update cr2.sh

--- cr2.sh
+++ cr2.sh
@@ -5,17 +5,17 @@ USERS=`awk -F: '{ print $1 }' < /etc/passwd`
 
 touch tmp
 chmod 000 tmp
-grep -a "$DATE" /var/log/auth.log | grep "Successful su for root by root" -n -2 | grep "session opened for user root by" > tmp
+grep -a $DATE /var/log/auth.log | grep -A 2 "Successful su for root by root" | grep "session opened for user root by" > tmp
 for u in $USERS; do
-	if [ "$u" == "root" ]; then
-		continue
-	fi
-	while read i; do
-		grep "$u" <<< "$i" >/dev/null
-		if [ "$?" == "0" ]; then
-			echo "$u" became root
-			break
-		fi
-	done < tmp
+        if [ "$u" == "root" ]; then
+                continue
+        fi
+        while read i; do
+                grep "$u" <<< "$i" > /dev/null 
+                if [ "$?" == "0" ]; then
+                        echo "$u" became root
+                        break
+                fi
+        done < tmp 
 done
 rm tmp
