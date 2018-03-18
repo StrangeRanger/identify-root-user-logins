c460c48a892b13bfadb70316ded16560e48e2f27
huntert <hunterjthompson@comcast.net>
2018-03-18 23:29:29 +0000

    edited README.md in RLSpy and made root_login_search.py identify users who use 'sudo -i' and 'sudo bash'

--- RLSpy/README.md
+++ RLSpy/README.md
@@ -1,1 +1,4 @@
-These two scripts work together in order to find out if a user has logged/used su to become root. (NOTE: This program needs work, in the sense that it can only identify users that have logged into root using "su". It does not detect users who have used "sudo bash" or "sudo -i")
+These two scripts work together in order to find out if a user has logged/used su to become root. (NOTE: There is one feature that needs to be added: looks back at least one week and tells you who and how many times a users logged in as root on each of those days... That way you are informed about the last 7 days and not just the current day.)
+
+Additional Security:
+____	- Say a user, let's call him Bob, wants to log into the root account but does not want anyone to know that is was him. If he executed "sudo su <another user>", becoming a different user on the system, then logged in as root, Bob would still be flagged. But if Bob used "sudo -i", the user he had logged into, would be flagged instead.
--- RLSpy/root_login_search.py
+++ RLSpy/root_login_search.py
@@ -13,9 +13,11 @@ tmp = open("tmp.txt", "w+") # creates and opens a text file called tmp.txt
 with open("/var/log/auth.log", "r") as txt: 
     for line in txt:
         if re.match("^%s.+" % date1, line) or re.match("^%s.+" % date2, line): # takes all lines in /var/log/auth.log that were made on the current day...
-            if "Successful su for root by root" in line: # then takes all lines that says <--...
+            if "Successful su for root by root" in line: # then takes all lines that says <--... # this will identify users who used "sudo su"
                 lines_gen = islice(txt, 2) # and the 2 lines below it...
                 tmp.writelines(lines_gen) # then writes them into the tmp.txt file
+            if re.match("^.+COMMAND=/bin/bash$", line): # this will identify users who use "sudo bash" and "sudo -i"
+                tmp.writelines(line)
 tmp.close()
 
 subprocess.call("./users.sh") # calls to and executes users.sh
@@ -35,10 +37,11 @@ with open("tmp.txt", "r") as txt:
                 if word + "\n" in users:
                     root_users.append(word)
                     login = True # someone has been detected logging in as root
+                    break # this break is placed here to prevent accidental miscount of times a user logged into the root account. it also prevents users who did not log in as root to be flasely tagged.
 
 for u in users: # goes through the users array where u = a single user
     x = 0 # tallies the total amount of times a single users logged into the root account
-    t = 0
+    t = 0 # keeps track of where r is in the root_users array and once it become the length of root_users, it starts back at the top for loop, moving onto the next user in users
     for r in root_users: # goes through the root_users array where r = a single user
         t += 1
         if u == r + "\n":
