8627ed77d78ac19aa5490655ebfffda64dbbf496
huntert <hunterjthompson@comcast.net>
2018-04-02 02:35:02 +0000

    added new notes to README.md. Added feature where the auth.log will be scanned up to 7 days prior of the current day.

--- RLSpy/README.md
+++ RLSpy/README.md
@@ -1,7 +1,8 @@
-These two scripts work together in order to find out if a user has logged/used su to become root. (NOTE: There is one feature that needs to be added: looks back at least one week and tells you who and how many times a users logged in as root on each of those days... That way you are informed about the last 7 days and not just the current day.)
+These two scripts work together in order to find out if a user has logged/used su to become root.
 
 Additional Security Notes:
 	- Say a user, let's call him Bob, wants to log into the root account but does not want anyone to know that is was him. If he executed "sudo su <another user>", becoming a different user on the system, then logged in as root, Bob would still be flagged. But if Bob used "sudo -i", the user he had logged into, would be flagged insteadi.
 
 Program Notes/Falts:
 	- If this program is to work correctly, the hostname of the computer or server MUST NOT contain the name of any user on the system: if you have a user named bob, the hostname can not be "bob-computer" or anything containing "bob". This will cause the program to falsely flag bob as a user who logged in as root or will double up on the amount of times he logged in as root if he used "sudo su". (NOTE: This will only occure if a user has been flagged as someone who has logged in as root.)
+	- The auth.log will be scanned up to 1 week.
--- RLSpy/root_login_search.py
+++ RLSpy/root_login_search.py
@@ -1,56 +1,65 @@
 import re
-import datetime
+from datetime import datetime, timedelta
 from itertools import islice
-import subprocess # allows the execution of users.bash from within a python script
+import subprocess
 import os
 
-# the two lines below changes date to unix/linux format(e.g. March  1 "or" March 20)
-date2 = datetime.datetime.now().strftime("%b  %-d")
-date1 = datetime.datetime.now().strftime("%b %d")
-tmp = open("tmp.txt", "w+") # creates and opens a text file called tmp.txt
+N = 7 
+def root_users():
+    N_days_ago = datetime.now() - timedelta(days=N) # checks for users who have logged in as root from 7 days ago to current day
+    # the two lines below changes date to unix/linux format(e.g. March  1 "or" March 20)
+    date2 = N_days_ago.strftime("%b  %-d")
+    date1 = N_days_ago.strftime("%b %d")
+    tmp = open("tmp.txt", "w+") # creates and opens a text file called tmp.txt
 
-with open("/var/log/auth.log", "r") as txt: 
-    for line in txt:
-        if re.match("^%s.+" % date1, line) or re.match("^%s.+" % date2, line): # takes all lines in /var/log/auth.log that were made on the current day...
-            if "Successful su for root by root" in line: # then takes all lines that says <--... # this will identify users who used "sudo su"
-                lines_gen = islice(txt, 2) # and the 2 lines below it...
-                tmp.writelines(lines_gen) # then writes them into the tmp.txt file
-            if re.match("^.+COMMAND=/bin/bash$", line): # this will identify users who use "sudo bash" and "sudo -i"
-                tmp.writelines(line)
-tmp.close()
+    with open("/var/log/auth.log", "r") as txt: 
+        for line in txt:
+            if re.match("^%s.+" % date1, line) or re.match("^%s.+" % date2, line): # takes all lines in /var/log/auth.log that were made on the current day...
+                if "Successful su for root by root" in line: # then takes all lines that says <--... # this will identify users who used "sudo su"
+                    lines_gen = islice(txt, 2) # and the 2 lines below it...
+                    tmp.writelines(lines_gen) # then writes them into the tmp.txt file
+                if re.match("^.+COMMAND=/bin/bash$", line): # this will identify users who use "sudo bash" and "sudo -i"
+                    tmp.writelines(line)
+    tmp.close()
+    
+    subprocess.call("./users.sh") # calls to and executes users.sh
+    
+    users = [] # a list/array of all known users on the system
+    with open("users", "r") as txt: # places all users that are in the users file into users
+        for u in txt:
+            users.append(u)
+    
+    login = False # at this moment, no one has been detected logging in as root
+    root_users = []
+    # checks the tmp.txt file to see if any known users are named within it
+    with open("tmp.txt", "r") as txt:
+        for line in txt:
+            for word in re.findall(r"\w+", line):
+                if word != "root": # this makes sure that if root is in a line, which it will always be, it won't add root to the root_users array
+                    if word + "\n" in users:
+                        root_users.append(word)
+                        login = True # someone has been detected logging in as root
+                        break # this break is placed here to prevent accidental miscount of times a user logged into the root account. it also prevents users who did not log in as root to be falsely tagged.
+    
+    print("On " + date1 + ":")
+    for u in users: # goes through the users array where u = a single user
+        x = 0 # tallies the total amount of times a single users logged into the root account
+        t = 0 # keeps track of where r is in the root_users array and once it become the length of root_users, it starts back at the top for loop, moving onto the next user in users
+        for r in root_users: # goes through the root_users array where r = a single user
+            t += 1
+            if u == r + "\n":
+                x += 1
+            if t == len(root_users):
+                if x >= 1:
+                    print("   " + u + "    became root " + str(x) + " times.")
+    
+    if login == False:
+        print("   No one became root")
+    
+    os.remove("users") # removes file users
+    os.remove("tmp.txt") # removes file tmp.txt
 
-subprocess.call("./users.sh") # calls to and executes users.sh
+for i in range(7):
+    N -= 1 # everytime root_users() has gone through its course, one date earlier will be scanned (May 7th will be scanned, next May 8th will be scanned, etc.)
+    root_users()
 
-users = [] # a list/array of all known users on the system
-with open("users", "r") as txt: # places all users that are in the users file into users
-    for u in txt:
-        users.append(u)
-
-login = False # at this moment, no one has been detected logging in as root
-root_users = []
-# checks the tmp.txt file to see if any known users are named within it
-with open("tmp.txt", "r") as txt:
-    for line in txt:
-        for word in re.findall(r"\w+", line):
-            if word != "root": # this makes sure that if root is in a line, which it will always be, it won't add root to the root_users array
-                if word + "\n" in users:
-                    root_users.append(word)
-                    login = True # someone has been detected logging in as root
-                    break # this break is placed here to prevent accidental miscount of times a user logged into the root account. it also prevents users who did not log in as root to be falsely tagged.
-
-for u in users: # goes through the users array where u = a single user
-    x = 0 # tallies the total amount of times a single users logged into the root account
-    t = 0 # keeps track of where r is in the root_users array and once it become the length of root_users, it starts back at the top for loop, moving onto the next user in users
-    for r in root_users: # goes through the root_users array where r = a single user
-        t += 1
-        if u == r + "\n":
-            x += 1
-        if t == len(root_users):
-            if x >= 1:
-                print(u + " became root " + str(x) + " times.")
-
-if login == False:
-    print("No one became root")
-
-os.remove("users") # removes file users
-os.remove("tmp.txt") # removes file tmp.txt
