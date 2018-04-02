171e713970dcb91e68b988c0f9dc47b656d7d750
huntert <hunterjthompson@comcast.net>
2018-04-02 21:48:51 +0000

    added more documentation to root_login_search.py and edited a line in README.md

--- RLSpy/README.md
+++ RLSpy/README.md
@@ -2,7 +2,7 @@ These two scripts work together in order to find out if a user has logged/used s
 
 Security Features/Notes:
 - Say a user, let's call him Bob, wants to log into the root account but does not want anyone to know that is was him. If he executed "sudo su *{another user}*", becoming a different user on the system, then logged in as root, Bob would still be flagged. But if Bob used "sudo -i", the user he had logged into, would be flagged instead.
-- The auth.log will be scanned up to 7 days prior to the current day.
+- The auth.log will be scanned up to 7 days worth of logs.
 
 Program Notes/Faults:
 - If this program is to work correctly, the hostname of the computer or server MUST NOT contain the name of any user on the system: if you have a user named bob, the hostname can not be "bob-computer" or anything containing "bob". This will cause the program to falsely flag bob as a user who logged in as root or will double up on the amount of times he logged in as root if he used "sudo su". (NOTE: This will only occure if a user has been flagged as someone who has logged in as root.)
--- RLSpy/root_login_search.py
+++ RLSpy/root_login_search.py
@@ -6,18 +6,18 @@ import os
 
 N = 7 
 def root_users():
-    N_days_ago = datetime.now() - timedelta(days=N) # checks for users who have logged in as root from 7 days ago to current day
+    N_days_ago = datetime.now() - timedelta(days=N) # determines what days in the auth.log will be scanned, starting from 7 to 0 days prior to the current day 
     # the two lines below changes date to unix/linux format(e.g. March  1 "or" March 20)
     date2 = N_days_ago.strftime("%b  %-d")
     date1 = N_days_ago.strftime("%b %d")
-    tmp = open("tmp.txt", "w+") # creates and opens a text file called tmp.txt
+    tmp = open("tmp.txt", "w+") 
 
     with open("/var/log/auth.log", "r") as txt: 
         for line in txt:
-            if re.match("^%s.+" % date1, line) or re.match("^%s.+" % date2, line): # takes all lines in /var/log/auth.log that were made on the current day...
-                if "Successful su for root by root" in line: # then takes all lines that says <--... # this will identify users who used "sudo su"
-                    lines_gen = islice(txt, 2) # and the 2 lines below it...
-                    tmp.writelines(lines_gen) # then writes them into the tmp.txt file
+            if re.match("^%s.+" % date1, line) or re.match("^%s.+" % date2, line): # 1. takes all lines in /var/log/auth.log that were made N days ago
+                if "Successful su for root by root" in line: # this will identify users who used "sudo su" # 2. then takes all lines that say "Successful su..."
+                    lines_gen = islice(txt, 2) # 3. and takes the 2 lines below it...
+                    tmp.writelines(lines_gen) # 4. then writes them into the tmp.txt file
                 if re.match("^.+COMMAND=/bin/bash$", line): # this will identify users who use "sudo bash" and "sudo -i"
                     tmp.writelines(line)
     tmp.close()
@@ -25,11 +25,11 @@ def root_users():
     subprocess.call("./users.sh") # calls to and executes users.sh
     
     users = [] # a list/array of all known users on the system
-    with open("users", "r") as txt: # places all users that are in the users file into users
+    with open("users", "r") as txt: # places all users that are in the users file into the users array
         for u in txt:
             users.append(u)
     
-    login = False # at this moment, no one has been detected logging in as root
+    login = False # at this moment, no one has been detected as a user who has logged in as root
     root_users = []
     # checks the tmp.txt file to see if any known users are named within it
     with open("tmp.txt", "r") as txt:
@@ -38,13 +38,13 @@ def root_users():
                 if word != "root": # this makes sure that if root is in a line, which it will always be, it won't add root to the root_users array
                     if word + "\n" in users:
                         root_users.append(word)
-                        login = True # someone has been detected logging in as root
+                        login = True # someone has been detected as someone who logged in as root
                         break # this break is placed here to prevent accidental miscount of times a user logged into the root account. it also prevents users who did not log in as root to be falsely tagged.
     
     print("On " + date1 + ":")
-    for u in users: # goes through the users array where u = a single user
+    for u in users: # goes through the users array where u = a single known user
         x = 0 # tallies the total amount of times a single users logged into the root account
-        t = 0 # keeps track of where r is in the root_users array and once it become the length of root_users, it starts back at the top for loop, moving onto the next user in users
+        t = 0 # keeps track of where variable r is in the root_users array and once it become the length of root_users, it starts back at the top for loop, moving onto the next user in users
         for r in root_users: # goes through the root_users array where r = a single user
             t += 1
             if u == r + "\n":
@@ -56,10 +56,10 @@ def root_users():
     if login == False:
         print("    No one became root")
     
-    os.remove("users") # removes file users
-    os.remove("tmp.txt") # removes file tmp.txt
+    os.remove("users")
+    os.remove("tmp.txt")
 
 for i in range(7):
-    N -= 1 # everytime root_users() has gone through its course, one date earlier will be scanned (May 7th will be scanned, next May 8th will be scanned, etc.)
+    N -= 1 # every time root_users() has gone through its course, one date earlier in auth.log will be scanned (May 7th will be scanned, next May 8th will be scanned, etc.)
     root_users()
 
