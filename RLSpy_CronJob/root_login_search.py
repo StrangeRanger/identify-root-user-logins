5714a6255e5aceb848f6e3f8547e337ee954627a
huntert <hunterjthompson@comcast.net>
2018-04-05 03:24:09 +0000

    Removed the need for users.sh/second file. 2. Hostname problem is no more a thing. The computer's/server's hostname can now include the name of a user on the system. 3. The problem that would prevent needed info to determine the user who logged in as root when is used in combination with a gui terminal, is now gone. 4. The program will now identify users who have logged in as root even if their account is deleted, (see README.md for more info). Though an unfortunate security problem/fault that has arisen. 1. The security feature that would prevent a user from blaming someone else for logging in as root, does not work anymore.

--- RLSpy_CronJob/root_login_search.py
+++ RLSpy_CronJob/root_login_search.py
@@ -1,71 +1,56 @@
-import re
+import sys
+#from os.path import exists
+import collections
 from datetime import datetime, timedelta
-from itertools import islice
-import subprocess
-import os
 
-N = 7 # determines number of days ago, -1, that will be scanned through in auth.log (Current day: March 29, variable: 8, will look at logs taken on March 22 through March 29)
+N = 7 # how many days
 log = open("root_login_log", "a")
 
-log.write("---auth.log scanned on " + str(datetime.now()) + "---\n")
-subprocess.call("./users.sh") # calls to and executes users.sh
+# the commented out section below is something that will be introduced later in the future
+#if exists("root_login_log"):
+#    log.write("---auth.log scanned on " + str(datetime.now()) + "---\n") 
+#else:
+#    sys.exit("ERROR: Please make sure you have correctly created the cronjob: `59 23 * * 6 cd {location of RLSpy_CronJob} && python3 root_login_search.py`. It is important that you include `cd {location of RLSpy_CronJob}`. Everything before that can be customized.")
 
-def identify_users():
-    N_days_ago = datetime.now() - timedelta(days=N) # determines what days in the auth.log will be scanned, starting from 7 to 0 days prior to the current day 
-    # the two lines below changes date to unix/linux format(e.g. March  1 "or" March 20)
-    date2 = N_days_ago.strftime("%b  %-d")
-    date1 = N_days_ago.strftime("%b %d")
-    tmp = open("tmp.txt", "w+") 
+log.write("---auth.log scanned on " + str(datetime.now()) + "---\n")
+
+def root_users():
+    today = datetime.now().date()
+    start_date = today - timedelta(days=N)
+    this_year = datetime.now().year
+    last_year = this_year - 1
+    days = collections.defaultdict(collections.Counter) ## ?1
 
     with open("/var/log/auth.log", "r") as txt: 
         for line in txt:
-            if re.match("^%s.+" % date1, line) or re.match("^%s.+" % date2, line): # 1. takes all lines in /var/log/auth.log that were made N days ago
-                if "Successful su for root by root" in line: # this will identify users who used "sudo su" # 2. then takes all lines that say "Successful su..."
-                    lines_gen = islice(txt, 2) # 3. and takes the 2 lines below it...
-                    tmp.writelines(lines_gen) # 4. then writes them into the tmp.txt file
-                if re.match("^.+COMMAND=/bin/bash$", line): # this will identify users who use "sudo bash" and "sudo -i"
-                    tmp.writelines(line)
-    tmp.close()
-    
-    users = [] # a list/array of all known users on the system
-    with open("users", "r") as txt: # places all users that are in the users file into the users array
-        for u in txt:
-            users.append(u)
-    
-    login = False # at this moment, no one has been detected as a user who has logged in as root
-    root_users = []
-    # checks the tmp.txt file to see if any known users are named within it
-    with open("tmp.txt", "r") as txt:
-        for line in txt:
-            for word in re.findall(r"\w+", line):
-                if word != "root": # this makes sure that if root is in a line, which it will always be, it won't add root to the root_users array
-                    if word + "\n" in users:
-                        root_users.append(word)
-                        login = True # someone has been detected as someone who logged in as root
-                        break # this break is placed here to prevent accidental miscount of times a user logged into the root account. it also prevents users who did not log in as root to be falsely tagged.
-    
-    log.write("On " + date1 + ":\n")
-    for u in users: # goes through the users array where u = a single known user
-        x = 0 # tallies the total amount of times a single users logged into the root account
-        t = 0 # keeps track of where variable r is in the root_users array and once it become the length of root_users, it starts back at the top for loop, moving onto the next user in users
-        for r in root_users: # goes through the root_users array where r = a single user
-            t += 1
-            if u == r + "\n":
-                x += 1
-            if t == len(root_users):
-                if x >= 1:
-                    log.write("    " + u + "     became root " + str(x) + " times.\n")
-    
-    if not login:
-        log.write("    No one became root\n")
-    
-    os.remove("tmp.txt")
+            matched = None ## ?
+            fields = line.split() 
+            date_str = " ".join(fields[0:2]) + " " 
+            try: ## ? -->2
+                date = datetime.strptime(date_str + str(this_year), "%b %d %Y").date()
+                if date > today: raise ValueError
+            except ValueError:
+                date = datetime.strptime(date_str + str(last_year), "%b %d %Y").date()
 
-for i in range(N):
-    N -= 1 # every time root_users() has gone through its course, one date earlier in auth.log will be scanned (May 7th will be scanned, next May 8th will be scanned, etc.)
-    identify_users()
+            if (date < start_date):
+                # too old for interest
+                continue ## ? <--2
+            # "user : TTY=tty/1 ; PWD=/home/user ; USER=root ; COMMAND=/bin/su
+            if fields[4] == "sudo:":
+                user = fields[5]
+                if user != "root" and fields[-3] == "USER=root" and fields[-1] in ("COMMAND=/bin/bash", "COMMAND=/bin/sh", "COMMAND=/bin/su"): ## why say fields [-1] in ("...? ## why bin/sh?
+                    days[date][user] += 1 ## ?1
 
-os.remove("users")
+    while start_date <= today:
+        log.write(start_date.strftime("On %b %d:\n"))
+        users = days[start_date]
+        if users:
+            for user,count in users.items(): # why user,count?
+                log.write("    " + str(user) + " became root " + str(count) + (" time\n" if count == 1 else " times\n"))
+        else:
+            log.write("    No one became root\n")
+        start_date += timedelta(days=1)
+
+root_users()
 log.write("*****************************************************\n")
 log.close()
-
