4c845cf9a51d845a756735b14de649eb9e5884ff
StrangeRanger <hunterjthompson@comcast.net>
2018-08-16 18:11:12 -0700

    changed one line of code the be more efficient

--- SU/sudo_users.py
+++ SU/sudo_users.py
@@ -4,11 +4,8 @@ user = str(input("What user are you looking for?\n"))
 txt = open("/etc/group", "r") # opens the group file in read mode
 
 for line in txt:
-    if re.match("(.*)sudo(.*)", line): # finds the sudo group
+    if line.startswith("sudo"): # finds the sudo group
         if re.match("sudo.+(:|,)%s(,|$)" % user, line): # checks if the given user is in the sudo group
             print(user + " is in the sudo group")
-            break
         else:
             print(user + " is NOT in the sudo group")
-            break
-
